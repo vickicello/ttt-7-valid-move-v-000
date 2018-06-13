fdf35694e594b1e40591b9f40c31681f504ba026
Vicki Hamm <vicki.cello@gmail.com>
2018-06-13 15:30:13 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,7 +1,7 @@
 def valid_move?(board, index)
-  if index == position_taken?
+  if board[index] == position_taken?
     false
-  elsif index == between?(0, 8) && index == !(position_taken?)
+  elsif board[index] == between?(0, 8) && board[index] == !(position_taken?)
     true
   end
 end
